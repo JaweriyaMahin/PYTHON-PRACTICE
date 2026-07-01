42d9dfe57b826d68b9bd5ba4047d5bf42ebd3efb
JaweriyaMahin <jaweriyamahin786@gmail.com>
2026-07-01 23:16:45 +0530

    day1

---
+++ python.py
@@ -0,0 +1,100 @@
+print("hello world")
+ #output: hello world
+
+
+print("python")
+#output: python
+
+print('hello')
+#output: hello
+
+
+print("Hello, World!")
+#output: Hello, World!
+
+print("Have a good day.")
+ #output: Have a good day.
+
+print("Learning Python is fun!")
+ #output:  Learning Python is fun!
+
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+ #output:  Hello World! it will print on the same line.
+
+print(3)
+#output: 3
+
+print(5080)
+#output: 5080
+
+print(3+5)
+#output: 8
+
+print("I am", 22, "years old.")
+#output: I am 22 years old.
+
+print(5*4) #this is a comment 
+#output: 20
+
+a, b = 15, 4
+print(a+b) #output: 19 addition
+print(a-b) #output: 11 subtraction
+print(a/b) #output: 3.75 division
+print(a//b) #output: 3 floor division
+print(a % b) #output: 3 modulus
+print(a**b) #output:50625 power
+x = 5
+y = "John"
+print(x)
+print(y)
+#output: 5 
+#output: jhon
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+#output: Orange
+#output: Banana
+#output: Cherry
+
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+#output: Orange 
+#output: Orange
+#output: Orange
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+#output: apple
+#output: banana
+#output: cherry
+
+x = "Python is awesome"
+print(x)
+#output: python is awesome
+
+z = "daily python practice"
+print(z)
+#output: daily python practice
+
+x = "Python"
+y = "is"
+z = "intresting"
+print(x, y, z)
+#output: python is intresting
+
+
+
+
+
