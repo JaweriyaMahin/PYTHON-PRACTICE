f569abbeb0cef5b4aafad65462560a093c874e4c
JaweriyaMahin <jaweriyamahin786@gmail.com>
2026-07-03 21:03:42 +0530

    day2inputpractice

---
+++ python day 2.py
@@ -0,0 +1,60 @@
+name = input("Enter your name: ")
+print("Hello", name)
+#enter your name: alex
+#output: hello alex
+
+
+first = input("First Name: ")
+last = input("Last Name: ")
+print("Full Name:", first, last)
+#first name: alex
+#last name: kelin
+#output: alex kelin
+
+
+age = input("Enter your age: ")
+print("Your age is", age)
+#enter your age: 25
+#output: your age is 25
+
+
+city = input("Enter your city: ")
+print("You live in", city)
+#enter your city: london
+#output: you live in london
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
+#enter first number: 10
+#enter second number:16
+#output: 26
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Difference =", a - b)
+#enter first number:20
+#enter second number:8
+#output: 12
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Product =", a * b)
+#enter first number:5
+#enter second number:6
+#output: 30
+
+
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+print("Area =", length * width)
+#enter length  8
+#enter width 4
+#output: 32
+
+
+
+
