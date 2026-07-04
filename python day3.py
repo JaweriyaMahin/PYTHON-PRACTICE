2f256520880d9f3fb67edece784801a172337cdc
JaweriyaMahin <jaweriyamahin786@gmail.com>
2026-07-04 21:53:02 +0530

    day02

---
+++ python day3.py
@@ -0,0 +1,303 @@
+# INT
+age = 22
+
+print("Age:", age)
+print(type(age))
+
+# Output: Age: 22
+# <class 'int'>
+
+apple_box1 = 15
+apple_box2 = 20
+
+total_apples = apple_box1 + apple_box2
+print("Total Apples:", total_apples)
+# Output:Total Apples: 35
+
+runs = 85
+bonus_runs = 15
+print("Final Score:", )
+# Output: Final Score: 100
+
+boys = 18
+girls = 22
+
+students = boys + girls
+
+print("Total Students:", students)
+# Output: Total Students: 40
+
+
+age = 22
+
+print(age)
+print(type(age))
+#output: 22  <class 'int'>
+
+x = 5
+print(type(x)) 
+#output: x <class 'int'>
+
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+#output: <class 'int'>
+
+# FLOAT
+
+price = 99.99
+
+print(price)
+print(type(price))
+#output:99.99  <class 'float'>
+
+
+
+price = 99.99
+
+print("Price:", price)
+print(type(price))
+
+# Output: Price: 99.99
+# <class 'float'>
+
+
+
+percentage = 88.5
+
+print("Percentage:", percentage)
+# Output:
+# Percentage: 88.5
+
+
+temperature = 36.7
+
+print("Body Temperature:", temperature)
+# Output:
+# Body Temperature: 36.7
+
+
+math = 85
+science = 90
+english = 80
+print("Average Marks:",)
+# Output:
+# Average Marks: 85.0
+
+
+height = 5.6
+print("Height:", height, "feet")
+# Output:
+# Height: 5.6 feet
+
+
+#STRING
+
+name = "sophia"
+print("Name:", name)
+print(type(name))
+# Output: Name: sophia
+# <class 'str'>
+
+
+first_name = "sophia"
+last_name = "philips"
+
+print(first_name + " " + last_name)
+# Output:
+# Sophia philips
+
+language = "Python"
+
+print("Favourite Language:", language)
+# Output:
+# Favourite Language: Python
+
+
+city = "Nagpur"
+
+print(city.upper())
+print(city.lower())
+# Output:
+# NAGPUR
+# nagpur
+
+message = "Welcome to Python"
+
+print(message)
+# Output:
+# Welcome to Python
+
+#boolen
+
+is_passed = True
+
+print(is_passed)
+print(type(is_passed))
+# Output:
+# True
+# <class 'bool'>
+
+
+age = 20
+print(age >= 18)
+# Output:
+# True
+
+password = "python123"
+entered_password = "python12"
+
+print(password == entered_password)
+# Output:
+# false
+
+
+num1 = 15
+num2 = 30
+print(num1 > num2)
+# Output:
+# False
+
+
+is_student = True
+print(is_student)
+print(type(is_student))
+#output: True  <class 'bool'>
+
+
+#list
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print(fruits)
+print(type(fruits))
+# Output:
+# ['Apple', 'Banana', 'Mango']
+# <class 'list'>
+
+
+colors = ["Red", "Blue", "Black"]
+
+print(colors[0])
+print(colors[2])
+# Output:
+# Red
+# Black
+
+
+fruits = ["Apple", "Banana"]
+
+fruits.append("Orange")
+print(fruits)
+# Output:
+# ['Apple', 'Banana', 'Orange']
+
+#tuple
+
+days = ("Monday", "Tuesday", "Wednesday")
+print(days)
+print(type(days))
+# Output:
+# ('Monday', 'Tuesday', 'Wednesday')
+# <class 'tuple'>
+
+
+student = ("marry", 22,)
+print(student)
+# Output: ('marry', 22,)
+
+#set
+
+numbers = {10, 20, 30}
+print(numbers)
+print(type(numbers))
+# Output:{10, 20, 30}
+# <class 'set'>
+
+
+numbers = {10, 20, 20, 30, 30, 40}
+print(numbers)
+# Output: {10, 20, 30, 40}
+
+fruits = {"Apple", "Banana"}
+fruits.add("Mango")
+print(fruits)
+# Output: {'Apple', 'Banana', 'Mango'}
+
+
+colors = {"Red", "Blue", "Green"}
+print(len(colors))
+# Output: 3
+
+
+student = {
+    "name": "Jaweriya",
+    "age": 22
+}
+
+print(student)
+print(type(student))
+
+# Output:{'name': 'sophia', 'age': 22}
+# <class 'dict'>
+
+
+student = {
+    "name": "marry",
+    "course": "Python"
+}
+
+print(student["course"])
+# Output: Python
+
+
+employee = {
+    "name": "Aman",
+    "salary": 50000,
+    "city": "Pune"
+}
+print(employee)
+# Output:{'name': 'Aman', 'salary': 50000, 'city': 'Pune'}
+
+
+#complex
+
+num = 3 + 4j
+print(num)
+print(type(num))
+# Output: (3+4j)
+# <class 'complex'>
+
+number = 10 + 2j
+
+print(number)
+# Output:(10+2j)
+
+
+num1 = 2 + 3j
+num2 = 4 + 5j
+result = num1 + num2
+print(result)
+# Output: (6+8j)
+
+#none
+
+data = None
+
+print(data)
+print(type(data))
+# Output: None
+# <class 'NoneType'>
+
+
+city = "mumbai"
+print(city)
+print(type(city))
+#output: mumbai
+# <class 'NoneType'>
+
+
+
