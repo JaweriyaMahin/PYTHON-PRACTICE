697de010f73d96ed4270bc726929713e8b560ff5
JaweriyaMahin <jaweriyamahin786@gmail.com>
2026-07-14 22:27:52 +0530

    DAY12

---
+++ python day 12.py
@@ -0,0 +1,139 @@
+def welcome():
+    print("Welcome to Python!")
+
+welcome()
+
+# Output:
+# Welcome to Python!
+
+
+def name():
+    print("My name is Ali")
+
+name()
+
+# Output:
+# My name is Ali
+
+
+def number():
+    print(100)
+
+number()
+
+# Output:
+# 100
+
+
+def add():
+    print(25 + 15)
+
+add()
+
+# Output:
+# 40
+
+
+def multiply():
+    print(8 * 6)
+
+multiply()
+
+# Output:
+# 48
+
+
+def subject():
+    print("Python Programming")
+
+subject()
+
+# Output:
+# Python Programming
+
+
+def color():
+    print("Blue")
+
+color()
+
+# Output:
+# Blue
+
+
+
+def city():
+    print("Mumbai")
+
+city()
+
+# Output:
+# Mumbai
+
+
+def even():
+    print(2, 4, 6, 8, 10)
+
+even()
+
+# Output:
+# 2 4 6 8 10
+
+
+def greeting():
+    print("Good Morning!")
+
+greeting()
+
+# Output:
+# Good Morning!
+
+
+
+def fruit():
+    print("Apple")
+
+fruit()
+
+# Output:
+# Apple
+
+
+def college():
+    print("ABC Engineering College")
+
+college()
+
+# Output:
+# ABC Engineering College
+
+
+def language():
+    print("Python")
+
+language()
+
+# Output:
+# Python
+
+
+def square():
+    print(9 * 9)
+
+square()
+
+# Output:
+# 81
+
+
+
+def success():
+    print("Function executed successfully!")
+
+success()
+
+# Output:
+# Function executed successfully!
+
+
+
